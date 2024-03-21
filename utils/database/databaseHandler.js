6c57c5044a28987716989d6c9614bb7238d21e70
Codies | Marcus S <43828282+tbfCodies@users.noreply.github.com>
2024-03-21 19:27:38 +0100

    Add files via upload

--- utils/database/databaseHandler.js
+++ utils/database/databaseHandler.js
@@ -1,38 +1,43 @@
-const sql = require('mssql');
+const mysql = require('mysql');
 require("dotenv").config();
 
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    server: process.env.DB_HOST,
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    options: {
-        encrypt: true,
-        enableArithAbort: true
-    }
+    port: 3306,
+    ssl: false
 };
 
-const pool = new sql.ConnectionPool(config);
+const pool = mysql.createPool(config);
 
 function connectDB() {
     try {
-        pool.connect(err => {
+        pool.getConnection((err, connection) => {
             if (err) {
-                console.error(err);
+                console.error("1" + err);
                 return;
             }
 
             console.log("Connected to database!");
+            // Don't forget to release the connection when done using it
+            connection.release();
         });
     } catch (e) {
-        console.error(e);
+        console.error("2" + e);
     };
 };
 
 function closeDB() {
     try {
-        pool.close();
-        console.log("Database connection closed.");
+        pool.end(err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log("Database connection closed.");
+        });
     } catch (e) {
         console.error(e);
     };
